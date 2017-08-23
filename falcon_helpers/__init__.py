d685833b6700a6c40da0e53b899e7825c3e6b51d
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2017-08-22 23:26:51 -0500

    Add a Jinja2 Middleware

--- falcon_helpers/__init__.py
+++ falcon_helpers/__init__.py
@@ -1,1 +1,2 @@
 from .middlewares.statics import StaticsMiddleware
+from .middlewares.jinja2 import Jinja2Middleware
--- falcon_helpers/middlewares/__init__.py
+++ falcon_helpers/middlewares/__init__.py
@@ -1,1 +1,6 @@
 from .statics import StaticsMiddleware
+from .jinja2 import (
+    Jinja2ConfigurationError,
+    Jinja2Middleware,
+    Jinja2Response,
+)
