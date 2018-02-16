3ae58945de0759728b726aae6ad891272cb5859f
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2018-02-15 23:36:18 -0600

    Add Pagination and Order to Crud List

--- falcon_helpers/resources/crud.py
+++ falcon_helpers/resources/crud.py
@@ -1,6 +1,8 @@
 import sqlalchemy as sa
 import falcon
 
+from ..utils import flatten
+
 
 class ListBase:
     """A base class for returning list of objects.
@@ -15,6 +17,8 @@ class ListBase:
     db_cls = None
     schema = None
 
+    default_order = None
+
     def on_get(self, req, resp):
         result = self.get_objects(req)
         schema = self.schema(many=True)
@@ -22,8 +26,44 @@ class ListBase:
         resp.status = falcon.HTTP_200
         resp.body = schema.dump(result)
 
+    def base_query(self):
+        return self.session.query(self.db_cls)
+
+    def pagination_hook(self, query, req):
+        """Create a hook for pagination"""
+        size = int(req.params.get('pageSize', 50))
+
+        # -1 here is so that the page numbers start at 1
+        page = int(req.params.get('page', 1)) - 1
+
+        if page < 0:
+            page = 0
+
+        return query.limit(size).offset((page * size))
+
+    def filter_hook(self, query, req):
+        return query
+
+    def order_hook(self, query, req):
+        request_order = req.params.get('sort_by', [])
+
+        # This takes advantage of falcons duplicate-param parsing which returns
+        # a list when it finds a param multiple times.
+        if type(request_order) != list:
+            request_order = [request_order]
+
+        request_order = list(flatten([x.split(';') for x in request_order]))
+        request_order = [sa.desc(x[1:]) if x.startswith('-') else sa.asc(x) for x in request_order ]
+
+        default_order = request_order or self.default_order or self.db_cls.__mapper__.primary_key
+        return query.order_by(*default_order)
+
     def get_objects(self, req):
-        return self.session.query(self.db_cls).all()
+        base = self.base_query()
+        order = self.order_hook(base, req)
+        paged = self.pagination_hook(order, req)
+
+        return paged.all()
 
 
 class CrudBase:
