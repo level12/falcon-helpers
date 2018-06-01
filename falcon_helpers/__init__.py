5e372c10c3e56ce46b45926929146b4aef43c967
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2018-06-01 17:35:28 -0500

    Add Config to root

--- falcon_helpers/__init__.py
+++ falcon_helpers/__init__.py
@@ -3,5 +3,6 @@ from .middlewares.jinja2 import Jinja2Middleware
 from .middlewares.auth_required import AuthRequiredMiddleware
 
 from .resources import auth0
+from .config import Config
 
 from . import contrib
