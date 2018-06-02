0686b70cbbd2529f0e689867f604cef5edd1618a
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2018-06-01 19:00:16 -0500

    Ensure the Extras are installed in CI

--- falcon_helpers/contrib/sentry.py
+++ falcon_helpers/contrib/sentry.py
@@ -3,6 +3,36 @@ import raven
 
 
 class Sentry:
+    """Simple Sentry integration plugin
+
+        config = {'sentry_dsn': 'some@sentry.io'}
+        app = falcon.API()
+        sentry = Sentry(config['sentry_dsn'])
+        sentry.register_app(sentry)
+
+    You can get access to this globally so that you can capture exceptions without it having to get
+    all the way up to the Falcon layer again.  Any exception that is caught by the falcon error
+    handler will be sent to sentry (except HTTPError's and HTTPStatus's).
+
+        sentry = Sentry()
+
+        def create_app(config):
+            app = falcon.API()
+
+            sentry.set_dsn(config.sentry_dsn)
+            sentry.register(app)
+            return app
+
+
+        class SomeResource:
+
+            def on_get(self, req, resp):
+                try:
+                    some_failure()
+                except Exception as e:
+                    sentry.captureException(e)
+                    pass
+    """
     def __init__(self, dsn=None):
         self.set_dsn(dsn)
 
