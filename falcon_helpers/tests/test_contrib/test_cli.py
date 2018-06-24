5c0fd9b654bdeac2c1df99e64ab5edfa74462c1d
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2018-06-24 14:34:54 -0500

    Add a custom request logger

--- falcon_helpers/tests/test_contrib/test_cli.py
+++ falcon_helpers/tests/test_contrib/test_cli.py
@@ -1,8 +1,10 @@
-import pytest
 import unittest.mock as mock
 
+import pytest
 import click.testing
+
 import falcon_helpers.contrib.cli as cli
+import falcon_helpers.contrib.wsgi as wsgi
 
 
 @pytest.fixture(scope='function')
@@ -24,25 +26,33 @@ def runner(request):
 def test_server_without_host_and_port(runner, cli_with_app):
     with mock.patch('falcon_helpers.contrib.cli.make_server') as m_make_server:
         runner.invoke(cli_with_app, ['server', 'start'])
-        m_make_server.assert_called_with('127.0.0.1', 9090, 'fake')
+        m_make_server.assert_called_with(
+            '127.0.0.1', 9090, 'fake',
+            handler_class=wsgi.CustomLoggingWSGIRequestHandler)
 
 
 def test_server_with_host(runner, cli_with_app):
     with mock.patch('falcon_helpers.contrib.cli.make_server') as m_make_server:
         runner.invoke(cli_with_app, ['server', 'start', '-h', 'localhost'])
-        m_make_server.assert_called_with('localhost', 9090, 'fake')
+        m_make_server.assert_called_with(
+            'localhost', 9090, 'fake',
+            handler_class=wsgi.CustomLoggingWSGIRequestHandler)
 
 
 def test_server_with_port(runner, cli_with_app):
     with mock.patch('falcon_helpers.contrib.cli.make_server') as m_make_server:
         runner.invoke(cli_with_app, ['server', 'start', '-p', '9'])
-        m_make_server.assert_called_with('127.0.0.1', 9, 'fake')
+        m_make_server.assert_called_with(
+            '127.0.0.1', 9, 'fake',
+            handler_class=wsgi.CustomLoggingWSGIRequestHandler)
 
 
 def test_server_with_long(runner, cli_with_app):
     with mock.patch('falcon_helpers.contrib.cli.make_server') as m_make_server:
         runner.invoke(cli_with_app, ['server', 'start', '--port', '9', '--host', 'localhost'])
-        m_make_server.assert_called_with('localhost', 9, 'fake')
+        m_make_server.assert_called_with(
+            'localhost', 9, 'fake',
+            handler_class=wsgi.CustomLoggingWSGIRequestHandler)
 
 
 def test_server_without_app(runner):
